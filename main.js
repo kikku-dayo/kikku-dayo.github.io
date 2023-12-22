b7c817fc07cc244de8595b8091b6f2b02fec92a4
kikku-dayo <kikku-dayo@users.noreply.github.com>
2023-12-21 21:55:18 -0500

    modified index.html | created main.js and style.css
    Made light progress on nav bar

---
+++ main.js
@@ -0,0 +1,19 @@
+document.body.onload = loadHeader;
+function loadHeader(){
+    const headerBox = document.createElement("div");
+    const header1 = document.createElement("h1");
+    //const para = document.createElement("p");
+    
+    headerBox.id = "header"
+
+    header1.innerHTML = "KKDY";
+    //para.innerHTML = "portfolio under construction"
+
+    document.body.appendChild(headerBox);
+    document.getElementById("header").appendChild(header1);
+    //document.getElementById("header").appendChild(para);
+}
+
+function navHome(){
+    document;
+}
